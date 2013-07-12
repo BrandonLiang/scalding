77bdcbad90896b3ae48bcfa13de91c8bce2fee0c
Koert Kuipers <koert@tresata.com>
2013-07-12 12:18:48 -0400

    fix eval-tool script

--- scalding-eval/test/SomeJob.scala
+++ scalding-eval/test/SomeJob.scala
@@ -1,6 +1,7 @@
 // run with:
 // hadoop jar scalding-eval/target/scala-2.9.2/scalding-eval-assembly-0.8.5.jar scalding-eval/test/SomeJob.scala --hdfs --input in.txt --output out
-
+// or:
+// scripts/eval-tool scalding-eval/test/SomeJob.scala --hdfs --input candidates.txt --output out
 import com.twitter.scalding._
 
 (args : Args) => {
